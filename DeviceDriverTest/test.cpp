8cf4049b8f2c163bd52462ef6d2f9052853ab956
JunhyeokChoi <anchovy1228@naver.com>
2023-07-07 11:38:11 +0900

    feature 1

--- DeviceDriverTest/test.cpp
+++ DeviceDriverTest/test.cpp
@@ -1,6 +1,12 @@
 #include <gtest/gtest.h>
+#include "../DeviceDriver/Application.cpp"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST(ApplicationTest, ReadAndPrint) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
 }
